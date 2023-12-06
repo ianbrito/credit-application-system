78c4eb48479b55730dfc06c8e8b76c19481d429d
Ian Azevedo <ianbazevedo@gmail.com>
2023-12-06 19:23:47 -0300

    fix: findByCreditCode in CreditService

--- src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
+++ src/main/kotlin/me/dio/credit/application/system/service/impl/CreditService.kt
@@ -28,7 +28,7 @@ class CreditService(
 
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
         val credit: Credit = this.creditRepository.findByCreditCode(creditCode)
-            ?: throw RuntimeException("Creditcode $creditCode not found")
+            ?: throw IllegalArgumentException("Creditcode $creditCode not found")
         return if (credit.customer?.id == customerId) credit else throw IllegalArgumentException("Contact admin")
     }
 
